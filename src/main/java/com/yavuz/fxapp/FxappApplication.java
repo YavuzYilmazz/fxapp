dc3b3d05e07e26a4aad9b670a74e119e86d51c3a
Yavuz Yilmaz <yavuz.yilmaz1@outlook.com>
2025-04-20 14:53:48 +0300

    Initial commit

---
+++ src/main/java/com/yavuz/fxapp/FxappApplication.java
@@ -0,0 +1,13 @@
+package com.yavuz.fxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxappApplication.class, args);
+    }
+
+}
